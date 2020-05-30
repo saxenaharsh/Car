d4c515193902a6818aaf0df2be360aff7eed45cb
saxenaharsh <harsh.saxena@gmail.com>
2020-05-30 13:23:59 +0530

    Add files via upload

---
+++ Classes/src/com/HarshSaxena/Main.java
@@ -0,0 +1,15 @@
+package com.HarshSaxena;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Car porsche = new Car();
+        Car holden = new Car();
+
+
+        porsche.setModel("911");
+        System.out.println("Model is " + porsche.getModel());
+
+    }
+}
